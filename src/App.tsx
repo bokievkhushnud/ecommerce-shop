7f0a07b5ef93495636d9cfab8f29d59381bdf619
Khushnud <bokiev.khushnud@mail.ru>
2023-08-22 06:28:15 +0600

    restring login when user is already logged in

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,60 @@
 import React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from 'react-router-dom';
 import CssBaseline from '@mui/material/CssBaseline';
 import LoginPage from './pages/Login';
 import theme from './theme/theme';
 import MainPage from './pages/MainPage';
 import MainLayout from './components/layouts/MainLayout';
 import NotFoundPage from './pages/NotFound';
-import { Provider } from 'react-redux';
-import store from './store';
-
-let router = createBrowserRouter([
-  {
-    path: '/login',
-    element: (
-      <MainLayout>
-        <LoginPage />
-      </MainLayout>
-    ),
-  },
-  // {
-  //   path: '/register',
-  //   element: (
-  //     <MainLayout>
-  //       <RegistrationPage />
-  //     </MainLayout>
-  //   ),
-  // },
-  {
-    path: '/',
-    element: (
-      <MainLayout>
-        <MainPage />
-      </MainLayout>
-    ),
-  },
-  {
-    path: '*',
-    element: <NotFoundPage />,
-  },
-]);
+import { useSelector } from 'react-redux';
+import { RootState } from './store';
 
 const App: React.FC = () => {
+  const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
+
+  let router = createBrowserRouter([
+    {
+      path: '/login',
+      element: isLoggedIn ? (
+        <Navigate to="/" replace />
+      ) : (
+        <MainLayout>
+          <LoginPage />
+        </MainLayout>
+      ),
+    },
+    // {
+    //   path: '/register',
+    //   element: (
+    //     <MainLayout>
+    //       <RegistrationPage />
+    //     </MainLayout>
+    //   ),
+    // },
+    {
+      path: '/',
+      element: (
+        <MainLayout>
+          <MainPage />
+        </MainLayout>
+      ),
+    },
+    {
+      path: '*',
+      element: <NotFoundPage />,
+    },
+  ]);
+
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <RouterProvider router={router} />
-      </ThemeProvider>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={router}></RouterProvider>
+    </ThemeProvider>
   );
 };
 
