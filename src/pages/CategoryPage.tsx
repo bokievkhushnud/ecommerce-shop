e2afc8565589f6df19e008475a40839e1479b2d4
juli_ko <yuliakolomiets09@gmail.com>
2023-09-19 00:58:30 +0300

    feat: implement infinite scroll for many products

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -15,11 +15,14 @@ import {
   queryAllProducts,
   searchProducts,
 } from '../commercetools-api/queryAllProducts';
+import { getCartByCustomerID } from '../commercetools-api/getCartByCustomerID';
+import { createCart } from '../commercetools-api/createCart';
 
 import ProductCard from '../components/common/ProductCard';
 import CategorySidebar from '../components/common/CategorySidebar';
 import BreadcrumbNavigation from '../components/common/BreadcrumbNavigation';
 import SearchAndFilter from '../components/common/SearchAndFilter';
+import { current } from '@reduxjs/toolkit';
 
 function CategoryPage() {
   const { id } = useParams<{ id?: string }>();
@@ -27,6 +30,9 @@ function CategoryPage() {
   const [productsData, setProductsData] = useState<IProduct[] | null>(null);
   const [selectedFilter, setSelectedFilter] = useState<string>('Default');
   const [searchTerm, setSearchTerm] = useState<string>('');
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [totalCount, setTotalCount] = useState<number>(0);
+  const [allowfetching, setAllowFetching] = useState<boolean>(true);
 
   const fetchProducts = async () => {
     let sortOrder = '';
@@ -39,19 +45,41 @@ function CategoryPage() {
     } else if (selectedFilter === 'Price: High to Low') {
       sortOrder = 'price.desc';
     }
-
-    if (searchTerm) {
-      const results = await searchProducts(searchTerm);
-      setProductsData(results);
-    } else {
-      const data = await queryAllProducts(sortOrder);
-      setProductsData(data);
+    if (allowfetching) {
+      if (searchTerm) {
+        console.log('fetching');
+        const results = await searchProducts(searchTerm);
+        setProductsData(results);
+      } else {
+        console.log('category choosen', category);
+        console.log('fetching');
+        const data = await queryAllProducts(sortOrder, currentPage, category);
+        setProductsData(data.results);
+        setTotalCount(data.total);
+      }
     }
   };
 
   useEffect(() => {
+    document.addEventListener('scroll', scrollHandler);
+    return function () {
+      document.removeEventListener('scroll', scrollHandler);
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log('new category/search/filter, fetch products', category);
     fetchProducts();
-  }, [searchTerm, selectedFilter]);
+  }, [category, searchTerm, selectedFilter, currentPage]);
+
+  useEffect(() => {
+    if (productsData) {
+      if (productsData.length === totalCount) {
+        console.log('stop fetching');
+        setAllowFetching(false);
+      }
+    }
+  }, [productsData, totalCount]);
 
   const defaultCategory: ICategory = {
     id: 'istdef',
@@ -63,11 +91,27 @@ function CategoryPage() {
   useEffect(() => {
     if (id) {
       getCategoryByID(id).then((data) => setCategory(data));
+      setCurrentPage(1);
+    } else {
+      setCategory(null);
     }
+    setAllowFetching(true);
   }, [id]);
 
   const handleFilterChange = (event: SelectChangeEvent<string>) => {
     setSelectedFilter(event.target.value);
+    setCurrentPage(1);
+    setAllowFetching(true);
+  };
+  const scrollHandler = () => {
+    if (
+      document.documentElement.scrollHeight -
+        (document.documentElement.scrollTop + window.innerHeight) <
+      50
+    ) {
+      console.log('scroll now');
+      setCurrentPage((prev) => prev + 1);
+    }
   };
 
   return (
