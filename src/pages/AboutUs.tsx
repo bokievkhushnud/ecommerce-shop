cb801de4eba8d985cba0206b244c53603062894e
Khushnud <bokiev.khushnud@mail.ru>
2023-09-21 00:40:39 +0600

    fix: small fixes

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,129 +1,93 @@
-import React from 'react';
-import { createTheme } from '@mui/material/styles';
-import { Box, Container, Typography, Link } from '@mui/material';
-import { ThemeProvider } from '@emotion/react';
-import { useTheme } from '@mui/material/styles';
+import { Box, Container, Typography, Link, Grid } from '@mui/material';
 import Card from '../components/specific/aboutTeamCard';
 import { teammatesData, bios } from '../assets/teammatesData';
-
 import photoIT from '../assets/images/Irina-photo.jpg';
 import photoKB from '../assets/images/Khushnud-photo.jpg';
 import photoYK from '../assets/images/Yulia-photo.jpg';
 
 const AboutUs = () => {
   const images = [photoIT, photoKB, photoYK];
-  const theme = useTheme();
-  const isDesktop = theme.breakpoints.up('md');
 
   return (
-    <>
-      <Container
-        component="section"
-        sx={{
-          margin: 0,
-          padding: '5% 0',
-          width: 1,
-          height: 1,
-        }}
-      >
-        <ThemeProvider theme={theme}>
-          <Box
-            sx={{
-              width: '100%',
+    <Container
+      component="section"
+      sx={{
+        py: '5%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Box sx={{ mb: 4 }}>
+        <Link href="https://rs.school/index.html" underline="none">
+          <img
+            src="https://rs.school/images/rs_school_js.svg"
+            alt="The Rolling Scopes School website"
+            style={{
+              border: '3px dashed #000',
               borderRadius: '50%',
+              width: 200,
+              height: 80,
+              display: 'block',
+              margin: '0 auto',
             }}
-          >
-            <Link href="https://rs.school/index.html" underline="none">
-              <img
-                src="https://rs.school/images/rs_school_js.svg"
-                alt="link The Rolling Scopes School website"
-                width={`${200}px`}
-                height={`${80}px`}
-                style={{ border: '3px dashed #000', margin: '0 auto' }}
-              />
-            </Link>
-            <div
+          />
+        </Link>
+      </Box>
+
+      <Box sx={{ maxWidth: '800px', mb: 4 }}>
+        <Typography
+          variant="body1"
+          gutterBottom
+          sx={{ fontSize: '1.1rem', textAlign: 'justify' }}
+        >
+          Grandma's Basket online shop website is a graduation work for
+          Javascript/Frontend course of The Rolling Scopes School' final task.
+        </Typography>
+        <Typography
+          variant="body1"
+          sx={{ fontSize: '1.1rem', textAlign: 'justify' }}
+        >
+          The Rolling Scopes School (RS School) is a free-of-charge and
+          community-based education program conducted by The Rolling Scopes
+          developer community since 2013. If you want to learn more about Rs
+          School and its courses, click on the RS School icon.
+        </Typography>
+      </Box>
+
+      {teammatesData.map((teammate, index) => (
+        <Grid
+          container
+          spacing={5}
+          justifyContent="center"
+          sx={{ mb: 4 }}
+          key={index}
+        >
+          <Grid item xs={12} sm={4}>
+            <img
+              src={images[index]}
+              alt="teammate"
               style={{
-                margin: '0 auto',
+                width: '100%',
+                maxWidth: '330px',
+                borderRadius: '15px',
               }}
-            >
-              <Typography
-                variant="body2"
-                sx={{
-                  backgroundColor: 'primary.light',
-                  padding: '1rem',
-                  textAlign: 'justify',
-                  fontSize: '1.1rem',
-                  margin: '0 auto',
-                }}
-              >
-                Grandma's Basket online shop website is a graduation work for
-                Javascript/Frontend course of The Rolling Scopes School' final
-                task.
-              </Typography>
-              <Typography
-                variant="body2"
-                sx={{
-                  backgroundColor: 'primary.light',
-                  padding: '1rem',
-                  textAlign: 'justify',
-                  fontSize: '1.1rem',
-                  margin: '0 auto',
-                }}
-              >
-                The Rolling Scopes School (RS School) is a free-of-charge and
-                community-based education program conducted by The Rolling
-                Scopes developer community since 2013. If you want to learn more
-                about Rs School and its courses, click on the RS School icon.
-              </Typography>
-            </div>
-          </Box>
-          <div style={{ margin: '3% auto' }}>
-            {teammatesData.map((teammate, index) => {
-              return (
-                <div style={{ marginBottom: '3%' }}>
-                  <Box
-                    className="card-contatiner"
-                    key={index + 1}
-                    sx={{
-                      display: 'flex',
-                      flexDirection: isDesktop ? 'column' : 'row',
-                    }}
-                  >
-                    <div className="card-photo">
-                      <img
-                        src={images[index]}
-                        alt="teammate"
-                        style={{
-                          width: '100%',
-                          height: '100%',
-                          maxWidth: '330px',
-                          maxHeight: '400px',
-                          minWidth: '200px',
-                          minHeight: '270px',
-                          borderRadius: '15px',
-                        }}
-                      />
-                    </div>
-                    <Card
-                      name={teammate.name}
-                      role={teammate.role}
-                      github={teammate.github}
-                      contributions={teammate.contributions}
-                    />
-                  </Box>
-                  <Box>
-                    <Typography sx={{ padding: '0 5px' }}>
-                      {bios[index]}
-                    </Typography>
-                  </Box>
-                </div>
-              );
-            })}
-          </div>
-        </ThemeProvider>
-      </Container>
-    </>
+            />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Card
+              name={teammate.name}
+              role={teammate.role}
+              github={teammate.github}
+              contributions={teammate.contributions}
+            />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Typography sx={{ textAlign: 'justify' }}>{bios[index]}</Typography>
+          </Grid>
+        </Grid>
+      ))}
+    </Container>
   );
 };
 
